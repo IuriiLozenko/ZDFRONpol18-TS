18a3d0fbdd2f1b9492d4a4548f6111d5f4169dbe
Kacper Żarna <kacper@192.168.1.16>
2022-12-12 21:32:01 +0100

    Kod 12.12.2022 21:30

---
+++ src/App.ts
@@ -0,0 +1,67 @@
+// 1. PODSTAWOWE TYPY DANYCH
+
+// a) number
+// let age = 32;
+// age = "lalalalalala";
+
+let age: number;
+age = 32;
+// age = "lalalala";
+// age = true;
+// age = {
+//   x: 1,
+// };
+age = 33;
+age = 34;
+//console.log(age);
+
+// b) string
+let surname: string;
+surname = "Wall";
+//surname = 15;
+
+// c) boolean
+let visible: boolean;
+visible = true;
+visible = false;
+// visible = 15;
+
+// 2. TYPOWANIE FUNKCJI
+
+const add = (a: number, b: number): number => a + b;
+
+const outcome = add(4, 5);
+console.log(outcome);
+
+const displayMessage = (message: string, age: number): void => {
+  alert(`${message}, ${age}`);
+};
+//displayMessage("My name is John", 33);
+
+// Zad 1.
+// Napisz funkcję która będzie obliczała cenę w zależności od tego czy zniżka jest przyznana czy nie.
+// Nazwa funkcji: calculatePrice
+// Parametry: originalPrice (liczba), hasDiscount (true/false)
+// Funkcjonalność: Jeżeli parametr hasDiscount jest równy true, zwróć liczbę originalPrice pomniejszoną o 23%, jeżeli hasDiscount jest równe false, zwróć z funkcji liczbę originalPrice nie przerabiając jej
+// const x = calculatePrice(100, true)
+
+const calculatePrice = (originalPrice: number, hasDiscount: boolean): number =>
+  hasDiscount ? originalPrice / 1.23 : originalPrice;
+
+const phonePrice = calculatePrice(2000, true);
+console.log(phonePrice);
+
+// 3. TYPY ELEMENTÓW DOM
+
+const button: HTMLButtonElement = document.querySelector("button");
+const categoriesList: HTMLUListElement = document.querySelector(".categories");
+
+button.addEventListener("click", () => {
+  // Zad 2.
+  // 1. W jakiś sposób wylosuj czy użytkownik ma zniżke czy jej nie ma i zapisz do zmiennej true albo false (Math.random())
+  const hasDiscount = Math.random() > 0.5 ? true : false; // Math.random zwraca liczbe od 0 1
+  // 2. Ściągnij wartość z inputu textowego id name (wybranie inputu + ściągnięcie value, samo value zapisz do zmiennej)
+  const inputValue = +document.querySelector("input").value;
+  // 3. Wykonaj console loga `You have to pay *tutaj cena obliczona przez funkcję calculatePrice, wywołaj ją tu wprowadzając jako argumenty wartości z pkt 2 i pkt 1.
+  console.log(`You have to pay ${calculatePrice(inputValue, hasDiscount)}`);
+});
